2a53d0932d8a133b4c504181c16cca2a94477a51
MawiyaManzar <mawiyamanzar28@gmail.com>
2025-09-04 20:32:16 +0530

    done some notable changes made a postCOmponent. Its Awesome

--- practise1/src/App.jsx
+++ practise1/src/App.jsx
@@ -7,29 +7,37 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      
+      <div style={{backgroundColor:'#dfe6e9',height:'100vh'}}>
+        <div style={{display:'flex', justifyContent:'center'}}>
+          <PostComponent/>
+          </div>  
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
+}
+
+const style={width:200,backgroundColor:"white",borderRadius:10,borderColor:"gray",padding:10}
+
+function PostComponent(){
+  return <div style={style}>  {/*This is father div*/}
+    <div style={{display:'flex'}} >{/* this is first child */}
+      <img src={'https://thf.bing.com/th/id/OSK.HEROB0d_ufZnWVweEGXnWvgK-NqrQp16Gd-V6vBZE9Yr91k?o=7&cb=thfc1rm=3&ucfimg=1&rs=1&pid=ImgDetMain&o=7&rm=3'}
+      style={{width:30, height:30,borderRadius:20}} 
+      />{/* image div first child*/}
+     <div style={{marginLeft:10,fontSize:10}}>{/*this is grandson has accountName,followerCount,TimeUploaded */}
+      <b>100xDevs</b>
+      <div>23000followers</div>
+        <div>12m</div>
+
+      
+     </div>
+    </div>
+   
+    <div style={{fontSize:12}}>{/* this is second child */}
+      this is caption of the post 
+    </div>
+  </div>
+  
 }
 
 export default App
