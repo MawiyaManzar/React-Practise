365d20856362595e1493a8b0bf5e4ad413bfde28
MawiyaManzar <mawiyamanzar28@gmail.com>
2025-09-05 21:54:29 +0530

    got error in latest commit full page waas blank

--- practise1/src/App.jsx
+++ practise1/src/App.jsx
@@ -2,14 +2,40 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {PostComponent} from './Post'
+// import {PostComponent} from './Post'
+
+function PostComponent({name,subtitle,description,time,image})
+        
+
+
+{
+  return <div style={style}>  {/*This is father div*/}
+    <div style={{display:'flex'}} >{/* this is first child */}
+      <img src={image}
+      style={{width:30, height:30,borderRadius:20}} 
+      />{/* image div first child*/}
+     <div style={{marginLeft:10,fontSize:10}}>{/*this is grandson has accountName,followerCount,TimeUploaded */}
+      <b>{name}</b>
+      <div>{subtitle} </div>
+
+        {time !== undefined ? <div style={{display:'flex'}}>
+        <div>{time}</div>
+        </div> : null}{/* This is conditional rendering if time is seen then its render if its a promoted post then not rendered  ?(works like a null*/}
+      
+     </div>
+    </div>
+   
+    <div style={{fontSize:12}}>{/* this is second child */}
+      {description}
+    </div>
+  </div>
+}
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
-  function addPost(){
-    
-  }
 
   return (
       <div style={{backgroundColor:'#dfe6e9',height:'100vh'}}>
