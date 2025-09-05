866143fc5f735c5686f8c9d795d128d10ab3db5e
MawiyaManzar <mawiyamanzar28@gmail.com>
2025-09-05 18:23:24 +0530

    added prop and components, next we have to add conditional rendering

--- practise1/src/App.jsx
+++ practise1/src/App.jsx
@@ -10,31 +10,41 @@ function App() {
       
       <div style={{backgroundColor:'#dfe6e9',height:'100vh'}}>
         <div style={{display:'flex', justifyContent:'center'}}>
-          <PostComponent/>
-          </div>  
+          <div>
+            <div><PostComponent
+            name={"WakeelSahab"}
+            subtitle={"1M "}
+            time={"2m Ago"}
+            image={"/symbol.png"}
+            description={"We are Building a product To revolutionize India Legal Industry. We believe in making Life easier for lawyer. Want to try our product?"}
+
+            /></div>
+            
+          </div>   
+        </div>  
       </div>
   )
 }
 
 const style={width:200,backgroundColor:"white",borderRadius:10,borderColor:"gray",padding:10}
 
-function PostComponent(){
+function PostComponent({name,subtitle,description,time,image}){
   return <div style={style}>  {/*This is father div*/}
     <div style={{display:'flex'}} >{/* this is first child */}
-      <img src={'https://thf.bing.com/th/id/OSK.HEROB0d_ufZnWVweEGXnWvgK-NqrQp16Gd-V6vBZE9Yr91k?o=7&cb=thfc1rm=3&ucfimg=1&rs=1&pid=ImgDetMain&o=7&rm=3'}
+      <img src={image}
       style={{width:30, height:30,borderRadius:20}} 
       />{/* image div first child*/}
      <div style={{marginLeft:10,fontSize:10}}>{/*this is grandson has accountName,followerCount,TimeUploaded */}
-      <b>100xDevs</b>
-      <div>23000followers</div>
-        <div>12m</div>
+      <b>{name}</b>
+      <div>{subtitle} follower</div>
+        <div>{time}</div>
 
       
      </div>
     </div>
    
     <div style={{fontSize:12}}>{/* this is second child */}
-      this is caption of the post 
+      {description}
     </div>
   </div>
   
